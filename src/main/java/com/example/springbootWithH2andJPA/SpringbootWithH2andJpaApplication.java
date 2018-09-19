66925b1a7bfde5228edb17cbb922ae824be58142
LebyanaWT <33281405+LebyanaWT@users.noreply.github.com>
2018-09-19 15:14:33 +0200

    first-commit

---
+++ src/main/java/com/example/springbootWithH2andJPA/SpringbootWithH2andJpaApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootWithH2andJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithH2andJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithH2andJpaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/springbootWithH2andJPA/SpringbootWithH2andJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springbootWithH2andJPA;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootWithH2andJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
